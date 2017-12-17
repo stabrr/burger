c6fc21be8fcf9f122e2d674ce3eb6bda671353fb
stabrr <bobbystabile@yahoo.com>
2017-12-16 20:33:11 -0600

    initial setup

---
+++ burgers_db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('The Mess');
+INSERT INTO burgers (burger_name) VALUES ('Big Boy');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
